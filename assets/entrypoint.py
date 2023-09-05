d5fabb5c48b5aa556fad0f3134ea47bdada5a545
Patrick Sadil <psadil@gmail.com>
2023-09-05 13:22:07 -0400

    ensure files created in working dir

--- assets/entrypoint.py
+++ assets/entrypoint.py
@@ -1,5 +1,20 @@
+import argparse
 from pathlib import Path
 
-file = Path("file").absolute()
-file.touch()
-Path("link").absolute().symlink_to(file)
+
+def main(root: Path) -> None:
+    file = root / "file"
+    file.touch()
+    link = root / "link"
+    link.symlink_to(file)
+
+    print(f"{file=}")
+    print(f"{link=}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("root", type=Path)
+
+    args = parser.parse_args()
+    main(root=args.root)
